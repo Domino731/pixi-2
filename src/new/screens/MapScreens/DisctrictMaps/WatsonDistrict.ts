a214cc50801a442238530c404e9a7f5a29b3c833
Domino731 <d.orzechowski18@gmail.com>
2024-03-30 13:36:50 +0100

    map - displaying posible entities

--- src/new/screens/MapScreens/DisctrictMaps/WatsonDistrict.ts
+++ src/new/screens/MapScreens/DisctrictMaps/WatsonDistrict.ts
@@ -2,8 +2,32 @@ import { DistrictMap, IDistrictEntity } from './DistrictMap';
 
 const WATSON_ENTITIES: Array<IDistrictEntity> = [
     {
-        name: 'Gun smith',
+        name: 'Gun shop',
         onClick: () => console.log('gun shop'),
+    },
+    {
+        name: 'Fixer',
+        onClick: () => console.log('Fixer'),
+    },
+    {
+        name: 'Pharmacy',
+        onClick: () => console.log('Pharmacy'),
+    },
+    {
+        name: 'Ripperdoc',
+        onClick: () => console.log('ripperdoc'),
+    },
+    {
+        name: 'Melee shop',
+        onClick: () => console.log('Melee shop'),
+    },
+    {
+        name: 'Netrunner',
+        onClick: () => console.log('Netrunner'),
+    },
+    {
+        name: 'scrapbooker',
+        onClick: () => console.log('scrapbooker'),
     },
 ];
 
