b1199fb37a96c2958871c838f6bb0244058cd5fa
Domino731 <d.orzechowski18@gmail.com>
2024-04-02 16:43:52 +0200

    auth - handling forms

--- src/main.ts
+++ src/main.ts
@@ -88,7 +88,8 @@ async function init() {
 }
 
 
-const auth = () => {
+const toggleForms = () => {
+    // Handle forms toggling
     const signInContainer = document.querySelector('#auth-sign-in-form') as HTMLElement;
     const signUpContainer = document.querySelector('#auth-sign-up-form') as HTMLElement;
     const passwordRecoveryContainer = document.querySelector('#auth-password-recovery-form') as HTMLElement;
@@ -112,6 +113,77 @@ const auth = () => {
         signUpContainer.style.display = 'none';
         passwordRecoveryContainer.style.display = 'block';
     }));
+};
+
+const signUp = () => {
+    const form = document.querySelector('#auth-sign-up-form') as HTMLElement;
+    // Errors
+    const emailError = document.querySelector('#sign-up-error-email') as HTMLElement;
+    const nickError = document.querySelector('#sign-up-error-nick') as HTMLElement;
+    const passwordError = document.querySelector('#sign-up-error-password') as HTMLElement;
+    const passwordRepeatError = document.querySelector('#sign-up-error-password-repeat') as HTMLElement;
+    // Inputs
+    const emailInput = document.querySelector('#sign-up-email') as HTMLInputElement;
+    const nickInput = document.querySelector('#sign-up-nick') as HTMLInputElement;
+    const passwordInput = document.querySelector('#sign-up-password') as HTMLInputElement;
+    const passwordRepeatInput = document.querySelector('#sign-up-password-repeat') as HTMLInputElement;
+
+    // TODO before release: make more detailed
+    const validateEmail = (): boolean => {
+        return true;
+        // return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(emailInput.value);
+    };
+    const validateNick = (): boolean => {
+        return true;
+        // return nickInput.value.length > 3;
+    };
+    const validatePasswordRepeatInput = () => {
+        return true;
+        // return passwordInput.value.length === passwordRepeatInput.value.length;
+    };
+    const validatePassword = () => {
+        return true;
+    };
+
+    const createNewAccount = () => {
+        console.log('create new account');
+    };
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const isEmailValid = validateEmail();
+        const isNickValid = validateNick();
+        const isPasswordRepeatValid = validatePasswordRepeatInput();
+        const isPasswordValid = validatePassword();
+        const isFormValid = isEmailValid && isNickValid && isPasswordValid && isPasswordRepeatValid;
+
+        if (isFormValid) {
+            createNewAccount();
+            return;
+        }
+
+    });
+};
+const signIn = () => {
+    const form = document.querySelector('#auth-sign-in-form') as HTMLFormElement;
+    const error = document.querySelector('#auth-sign-in-form-error') as HTMLElement;
+    const emailInput = document.querySelector('#sign-in-email') as HTMLElement;
+    const passwordInput = document.querySelector('#sign-in-password') as HTMLElement;
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        console.log({
+            emailInput,
+            passwordInput,
+        });
+        error.style.display = 'block';
+    });
+};
+
+const auth = () => {
+    toggleForms();
+    signUp();
+    signIn();
 };
 
 auth();
