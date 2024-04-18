925b212af091053b1765c0c9e35bb43183ad3abf
Domino731 <d.orzechowski18@gmail.com>
2024-04-18 15:15:02 +0200

    feature - map - dragging

--- src/new/screens/MapScreens/Map.ts
+++ src/new/screens/MapScreens/Map.ts
@@ -1,6 +1,7 @@
 import { Container } from 'pixi.js';
 import { MapPanel } from './MapPanel/MapPanel';
 import { Map2000 } from './Maps/2000/Map2000';
+import { Map500 } from './Maps/500/Map500';
 
 export class CityMap extends Container {
     private mapPanel: MapPanel;
@@ -9,9 +10,9 @@ export class CityMap extends Container {
         super();
         this.mapPanel = new MapPanel();
         // this.addChild(new Map200());
-        // this.addChild(new Map500());
+        this.addChild(new Map500());
         // this.addChild(new Map1000());
-        this.addChild(new Map2000());
+        // this.addChild(new Map2000());
         this.addChild(this.mapPanel);
 
 
