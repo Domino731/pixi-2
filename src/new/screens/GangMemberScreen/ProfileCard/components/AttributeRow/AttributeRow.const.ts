6dac67d05b737e6f4ee3eb0b09b09ee786418394
Domino731 <d.orzechowski18@gmail.com>
2024-05-14 17:09:44 +0200

    feature - gang member screen - attributes icons

---
+++ src/new/screens/GangMemberScreen/ProfileCard/components/AttributeRow/AttributeRow.const.ts
@@ -0,0 +1,5 @@
+import { GAME_COLORS } from '../../../../../../const/styles';
+
+export const CONFIG = {
+    BACKGROUND: GAME_COLORS.black2,
+};
--- src/new/screens/GangMemberScreen/ProfileCard/components/AttributeRow/AttributeRow.tsx
+++ src/new/screens/GangMemberScreen/ProfileCard/components/AttributeRow/AttributeRow.tsx
@@ -1,5 +1,6 @@
 import { Container, Graphics, Text } from 'pixi.js';
 import { AttributeRowOptions } from './AttributeRow.types';
+import { CONFIG } from './AttributeRow.const';
 
 export class AttributeRow extends Container {
     private readonly frameWidth: number;
@@ -17,14 +18,14 @@ export class AttributeRow extends Container {
     private createLabel() {
         const text = new Text(this.attributeName, {
             fill: 'white',
-            fontSize: 30,
+            fontSize: 24,
         });
         return text;
     }
 
     private createFrame() {
         const g = new Graphics();
-        g.beginFill('green');
+        g.beginFill(CONFIG.BACKGROUND);
         g.drawRect(0, 0, this.frameWidth, 40);
         g.endFill();
         return g;
