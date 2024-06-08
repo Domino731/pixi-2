b455b2daf3013cfcec636987bd93387b67f5d9f0
Domino731 <d.orzechowski18@gmail.com>
2024-06-08 16:22:09 +0200

    feature - cloth items - Cloth config

--- src/modules/items/Clothes.ts
+++ src/modules/items/Clothes.ts
@@ -1,23 +1,74 @@
 import { clothesConfigs } from './Clothes.const';
+import {
+    ClothGeneralData,
+    ClothModifier,
+    ClothOptions,
+    ClothQuote, ClothSex, ClothSexUnion,
+} from './Clothes.types';
+import { Sprite, Texture } from 'pixi.js';
 
 export class Cloth {
-    public readonly id: string;
+    private readonly id: string;
+    private generalData: ClothGeneralData;
+    private description: string | null;
+    private modifiers: ClothModifier;
+    private name: string;
+    private overview: string;
+    private quote: ClothQuote;
+    private textureFemaleSrc: string;
+    private textureMaleSrc: string;
 
+    constructor(options: ClothOptions) {
+        this.id = options.id;
+        this.generalData = options.generalData;
+        this.description = options.description;
+        this.modifiers = options.modifiers;
+        this.name = options.name;
+        this.overview = options.overview;
+        this.quote = options.quote;
+        this.textureFemaleSrc = options.textureFemaleSrc;
+        this.textureMaleSrc = options.textureMaleSrc;
+    }
 
-    constructor() {
+    private getTexture(sex: ClothSexUnion) {
+        return Texture.from(sex === ClothSex.male ? this.textureMaleSrc : this.textureFemaleSrc);
+    }
+
+    private getSprite(sex: ClothSexUnion) {
+        return new Sprite(Texture.from(sex === ClothSex.male ? this.textureMaleSrc : this.textureFemaleSrc));
     }
 }
 
 export class ClothesItems {
-    private static clothes: Cloth[] = [];
+    private static clothes: Record<string, Cloth> = ClothesItems.createClothes();
 
     constructor() {
     }
 
+    private static createClothes() {
+        const clothes: Record<string, Cloth> = {};
+        Object.values(clothesConfigs).map(el => {
+            clothes[el.id] = new Cloth({
+                id: el.id,
+                // @ts-ignore
+                generalData: el.general,
+                modifiers: el.modifier,
+                name: el.name,
+                overview: el.overview,
+                quote: {
+                    // @ts-ignore
+                    text: el.quote?.text ?? '',
+                    // @ts-ignore
+                    author: el.quote?.author ?? '',
+                },
+                textureMaleSrc: el.textureMale,
+                textureFemaleSrc: el.textureFemale,
+            });
+        });
+        return clothes;
+    }
 
     public static get(id: string) {
-        console.log(clothesConfigs);
-        const payload = this.clothes.find(el => el.id === id);
-        return payload;
+        return ClothesItems.clothes[id];
     }
 }
---
+++ src/modules/items/Clothes.types.ts
@@ -0,0 +1,53 @@
+import { Cloth } from './Clothes';
+
+export enum ClothesType {
+    feet = 'feet',
+    head = 'head',
+    innerTorso = 'innerTorso',
+    legs = 'legs',
+    outerTorso = 'outerTorso'
+}
+
+export type ClothesTypeUnion = keyof typeof ClothesType;
+
+export enum ClothesStyle {
+    KITSCH = 'KITSCH',
+    NEOMILITARISM = 'NEOMILITARISM',
+    ENTROPISM = 'ENTROPISM',
+    NEOKITSCH = 'NEOKITSCH'
+
+}
+
+export type ClothesStyleUnion = keyof typeof ClothesStyle;
+
+export interface ClothGeneralData {
+    type: ClothesTypeUnion,
+    style: ClothesStyleUnion
+    rarityTier: number
+}
+
+export type ClothModifier = Record<string, any>
+
+export interface ClothQuote {
+    text: string;
+    author: string | null;
+}
+
+export interface ClothOptions {
+    id: string;
+    generalData: ClothGeneralData;
+    description: string | null;
+    modifiers: ClothModifier;
+    name: string;
+    overview: string;
+    quote: ClothQuote;
+    textureFemaleSrc: string;
+    textureMaleSrc: string;
+}
+
+export enum ClothSex {
+    male = 'male',
+    female = 'female'
+}
+
+export type ClothSexUnion = keyof typeof ClothSex;
